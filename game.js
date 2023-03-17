9ad97c5152448a5493d9d8c272c43fea679d4a83
VillePisara <62200525+VillePisara@users.noreply.github.com>
2023-03-17 20:44:21 +0200

    Update game.js

--- game.js
+++ game.js
@@ -29,12 +29,44 @@ function preload() {
     this.load.image('banana', 'banana.png');
     this.load.image('platform', 'platform.png');
     this.load.audio('pickup', 'pickup.wav');
+    this.load.image('sky', 'sky.png');
+    this.load.image('player', 'player.png');
 }
 
 function create() {
 
-
     this.player = this.physics.add.sprite(100, 450, 'player');
+
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'platform');
+    platforms.create(50, 250, 'platform');
+    platforms.create(750, 220, 'platform');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+
+    bananas = this.physics.add.group({
+        key: 'banana',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    bananas.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(bananas, platforms);
+
+    this.physics.add.overlap(player, bananas, collectBanana, null, this);
 
     
     // Set up game objects here
